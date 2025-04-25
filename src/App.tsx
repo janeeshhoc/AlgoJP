124eed1c249710c39c6068057e49523a7a45c34c
Janeesh <janeesh@houseofcard.in>
2025-04-25 12:00:54 +0530

    l

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import PathfindingVS from "./Pages/PathfindingVS";
-import SortingApp from "./Pages/Sorting";
 import NQueen from "./Pages/N_Queen";
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/path-finding" element={<PathfindingVS />} />
         <Route path="/nqueens" element={<NQueen />} />
-        <Route path="/sorting" element={<SortingApp />} />
       </Routes>
     </div>
   );
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,9 +3,8 @@ import Card from "../components/Card";
 import Navbar from "../components/Navbar";
 import "../styles/home.css"; // Tailwind CSS is typically imported here
 import img1 from "../icons/path-find.jpg";
-import img2 from "../icons/sort.jpg";
 import img4 from "../icons/Chess.jpg";
-import desktopImage from "../icons/desk.jpg"; // Desktop image
+import desktopImage from "../icons/desk.png"; // Desktop image
 import { Link } from "react-router-dom";
 
 function Home() {
@@ -27,18 +26,9 @@ function Home() {
         backgroundImage: isDesktop ? `url(${desktopImage})` : "none",
       }}
     >
-      <Navbar msg={"Janeesh P"} />
-
-      <h2 className="text-2xl md:text-3xl text-center text-stone-200 py-4 relative z-10 bg-opacity-75 p-2 rounded-md">
-        Decode The Algorithms Like A Pro
-      </h2>
-
       <div className="cards-container grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 relative z-10">
         <Link className="no-underline" to="/path-finding">
           <Card array={[img1, "Path-Finder"]} />
-        </Link>
-        <Link className="no-underline" to="/sorting">
-          <Card array={[img2, "Sorting Algorithms"]} />
         </Link>
         <Link className="no-underline" to="/nqueens">
           <Card array={[img4, "N queens problem"]} />
